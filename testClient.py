beaa331c3ca57be6dd4cd18b8df72b407d9d2d5e
Jake <jdh5053@yahoo.com>
2017-12-07 23:21:18 -0500

    First Commit

---
+++ testClient.py
@@ -0,0 +1,52 @@
+import socket
+import thread
+import sys
+
+def recv_data():
+    "Receive data from other clients connected to server"
+    while 1:
+        try:
+            recv_data = client_socket.recv(4096)            
+        except:
+            #Handle the case when server process terminates
+            print "Server closed connection, thread exiting."
+            thread.interrupt_main()
+            break
+        if not recv_data:
+                # Recv with no data, server closed connection
+                print "Server closed connection, thread exiting."
+                thread.interrupt_main()
+                break
+        else:
+                print "Received data: ", recv_data
+
+def send_data():
+    "Send data from other clients connected to server"
+    while 1:
+        send_data = str(raw_input("Enter data to send (q or Q to quit):"))
+        if send_data == "q" or send_data == "Q":
+            client_socket.send(send_data)
+            thread.interrupt_main()
+            break
+        else:
+            client_socket.send(send_data)
+        
+if __name__ == "__main__":
+
+    print "*******TCP/IP Chat client program********"
+    print "Connecting to server..."
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((sys.argv[1], int(sys.argv[2])))
+
+    print "Connected to server.."
+
+    thread.start_new_thread(recv_data,())
+    thread.start_new_thread(send_data,())
+
+    try:
+        while 1:
+            continue
+    except:
+        print "Client program quits...."
+client_socket.close() 
